8e57821ffd30b504806ae13dca52cfd243898417
xIveilx <xIveilx@gmail.com>
2023-10-26 19:10:07 +0200

    background update

--- 1. Project/js/background.js
+++ 1. Project/js/background.js
@@ -10,60 +10,84 @@ window.addEventListener('resize', function(){
     canvas.height = window.innerHeight;
 })
 
-function Circle(x, y, r, speed){
+var hatarx = 100;
+var hatary = 1.01;
+//körök mennyisége:
+var korok = window.innerWidth /25;
+
+function Circle(x, y, r, speed, id){
+    this.id = id;
     this.speed = speed;
     this.x = x;
     this.y = y;
     this.r = r;
 
     this.draw = function(){
+        //c.beginPath();
+        //c.arc(this.x, this.y, this.r, 0, Math.PI * 2 , false);
+        //c.strokeStyle = '#074973';
+        //c.stroke();
+        //c.fillStyle = '#08678C';
+        //c.fill();
         c.beginPath();
-        c.arc(this.x, this.y, this.r, 0, Math.PI * 2 , false);
+        c.arc(this.x, this.y, this.r, 0, Math.PI / 2 , false);
+        c.arc(this.x, this.y + this.r * 2, this.r, Math.PI * 1.5,  0, false);
+        c.arc(this.x + this.r * 2, this.y + this.r * 2, this.r, Math.PI ,  Math.PI * 1.5, false);
+        c.arc(this.x + this.r * 2, this.y, this.r, Math.PI / 2,  Math.PI, false);
         c.strokeStyle = '#074973';
         c.stroke();
-        c.fillStyle = '#08678C';
+        c.fillStyle = '#074973';
         c.fill();
+        //c.beginPath();
+        //c.translate(this.x + this.r, this.y + this.r);
+        //c.lineTo(-100,-100);
+        //c.strokeStyle = '#074973';
+        //c.stroke();
     }
 
     this.update = function(){
-        if(this.y + this.r < innerHeight){
-            this.dy = -this.speed;
-        }
-        this.y += this.speed;
-        if(this.y + this.r > innerHeight){
-            circleArray = [];
+        //iránya:
+        this.x += this.speed;
+        this.y += this.speed * 0.23;
+        //reset:
+        if(this.y + this.r > innerHeight || this.x + this.r > innerWidth){
+            this.x = 0;//Math.random() * (window.innerWidth * 2) / hatarx;
+            this.y = Math.random() * (window.innerHeight * 2) -600;
         }
 
         this.draw();
     }
 }
-
+//Egy tömb amiben a körök vannak:
 var circleArray = [];
+//körök idja:
+var id = 0;
 
 function init(){
-    circleArray = [];
-
-    for (var i = 0; i < 10; i++) {
-        var speed = (Math.random() * 3)+1;
+        //adatok
+        var speed = (Math.random() * 8)+4;
         var r = 10;
         var x = Math.random() * (window.innerWidth - r * 2) + r;
-        var y = 10;
-        circleArray.push(new Circle(x, y, r, speed));
-    }
+        var y = Math.random() * (window.innerHeight - r * 2) + r;
+        circleArray.push(new Circle(x, y, r, speed, id));
+        console.log(id);
 }
 
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0, innerWidth, innerHeight);
-
+    
     for(var i = 0; i < circleArray.length;i++){
         circleArray[i].update();
     }
     
     if(circleArray.length == 0){
+        circleArray = [];
+        for (var i = 0; i < korok; i++) {
+        id = i;
         init();
+        }
     }
 }
 
-init();
 animate();
